722080cf3a27594921526ee684cb09c64997e2a4
mai <62747541+penzz1607@users.noreply.github.com>
2020-05-12 00:13:49 +0700

    123

--- src/SDL_utils.cpp
+++ src/SDL_utils.cpp
@@ -21,13 +21,13 @@ void waitUntilKeyPressed()
 int kt_play(int kt1)
 {
     int kt= kt1;
-    SDL_Event e;
+    SDL_Event c;
     while(true)
     {
-        if(SDL_WaitEvent(&e)!=0)
+        if(SDL_WaitEvent(&c)!=0)
         {
-            if(e.type == SDL_QUIT||e.key.keysym.sym==27) { kt++; break;}// nhấn phím éc để thoát hoặc tắt cửa sổ
-            if(e.key.keysym.sym==114) { kt=0; break; }// nhấn phím r để chơi lại
+            if(c.type == SDL_QUIT||c.key.keysym.sym==27) { kt++; break;}// nhấn phím éc để thoát hoặc tắt cửa sổ
+            if(c.key.keysym.sym==114) { kt=0; break; }// nhấn phím r để chơi lại
         }
     }
     return kt;
